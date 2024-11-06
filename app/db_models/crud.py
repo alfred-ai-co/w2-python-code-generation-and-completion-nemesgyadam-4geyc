4f49bff45160927f0c69fce652da662e9c0532f1
Adam Gyula Nemes <nemesgyadam@gmail.com>
2024-11-06 10:02:31 +0100

    Update crud.py

--- app/db_models/crud.py
+++ app/db_models/crud.py
@@ -3,3 +3,9 @@ from app.db_models.base import Project, Ticket
 
 
 # CRUD operations for Project
+def create_project(db: Session, name: str, description: str):
+    new_project = Project(name=name, description=description)
+    db.add(new_project)
+    db.commit()
+    db.refresh(new_project)
+    return new_project
